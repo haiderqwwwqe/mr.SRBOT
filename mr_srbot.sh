e4da2dc5be347b559136eb9c12794464175a990d
haiderqwwwqe <hiaderqwwwq6@gmail.com>
2017-03-03 17:48:21 +0300

    Update and rename x7_bot.sh to mr_srbot.sh

--- mr_srbot.sh
+++ mr_srbot.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-cd $HOME/x7_bot
+cd $HOME/mr_srbot
 
 install() {
 	    cd tg
@@ -67,8 +67,8 @@ fi
 #  --permanent-peer-ids                 use permanent peer ids
 #			#			#			#			#			#			#			#			#			#
 #Example To launch with second profile :
-# ./x7_bot.sh -p second-profile
+# ./mr_srbot.sh -p second-profile
 
 #			  OR
 
-# ./x7_bot.sh --profile second-profile
+# ./mr_srbot.sh --profile second-profile
